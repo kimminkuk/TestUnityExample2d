75bda220cc66298dd8bc91da158a1aaaff6032a4
kimminkuk <rlaal567@gmail.com>
2021-07-09 21:09:09 +0900

    Modify to Projectile for destroy room polygon

--- Objects/Projectile.cs
+++ Objects/Projectile.cs
@@ -37,6 +37,9 @@ public class Projectile : MonoBehaviour
 
     public void OnTriggerEnter2D(Collider2D other)
     {
-        Destroy(this.gameObject);
+        if (!other.isTrigger)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
